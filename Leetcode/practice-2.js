6f7d073ea015f27b555df776685db2ae1ac7bb9c
mansi <vaghasiyamaahigmail.com>
2024-02-17 09:21:08 +0530

    17/2/2024

--- Leetcode/practice-2.js
+++ Leetcode/practice-2.js
@@ -1,3 +1,9 @@
+// definition :-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order
+
 // example - 1
 // function twoSum(arr, target) {
 //     let n = arr.length;
@@ -14,7 +20,13 @@
 // console.log(twoSum([2,7,11,15],9));
 
 
-// example-7
+
+
+// defination:-Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+// Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+ // example-7
 {
     let n = 123;
     let rev = 0;
